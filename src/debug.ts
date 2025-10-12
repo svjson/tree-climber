c3a06c03a5ae54aebf40ebf8c9a681e6a2ad14a6
Sven Johansson <sven.johansson@iteam.se>
2025-10-12 23:42:05 +0200

    Pad node text in outputRecursive

--- src/debug.ts
+++ src/debug.ts
@@ -3,7 +3,7 @@ import { SyntaxNode } from 'tree-sitter'
 export const outputRecursive = (node: SyntaxNode, depth = 0) => {
   const padding = ' '.repeat(depth * 2)
   console.log(`${padding} - Node Type: '${node.type}'`)
-  console.log(node.text)
+  console.log(`${padding}   '${node.text}'`)
 
   for (const child of node.children) {
     outputRecursive(child, depth + 1)
